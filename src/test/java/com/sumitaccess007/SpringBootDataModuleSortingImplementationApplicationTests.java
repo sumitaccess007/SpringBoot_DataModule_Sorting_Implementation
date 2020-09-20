7bd8f7ba76792c7aa464c6c107b394a2adecc548
sumitaccess007 <sumitaccess007@gmail.com>
2020-09-20 20:21:12 +0530

    Fixed the issues

--- src/test/java/com/sumitaccess007/SpringBootDataModuleSortingImplementationApplicationTests.java
+++ src/test/java/com/sumitaccess007/SpringBootDataModuleSortingImplementationApplicationTests.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Sort;
-import org.w3c.dom.stylesheets.LinkStyle;
 
 import java.util.List;
 
